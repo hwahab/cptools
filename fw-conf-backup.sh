a49e70054396fc281d783141a3a98eed1ddda8a6
Michael Goessmann Matos <dj0nz@users.noreply.github.com>
2017-11-30 13:37:36 +0100

    backup multiple firewalls using cprid

--- fw-conf-backup.sh
+++ fw-conf-backup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # firewall configuration backup using cprid
+# run on checkpoint management server
+#
+# also see sk101047
+#
 # mgo 2017
-
 
 . /opt/CPshared/5.0/tmp/.CPprofile.sh
 
-FW=( fw1 fw2 fw3 fw4 )
+FW_IP=( 1.1.1.1 2.2.2.2 3.3.3.3 4.4.4.4 )
 BKP_DAY=`date +%W`
 
-for i in "${FW[@]}"; do
+for i in "${FW_IP[@]}"; do
    echo "Backup Firewall Configuration $i:"
    $CPDIR/bin/cprid_util -server $i -verbose rexec -rcmd clish -c "show configuration" > $i-$BKP_DAY
    $CPDIR/bin/cprid_util -server $i -verbose rexec -rcmd cpinfo -y all -i >> $i-$BKP_DAY
